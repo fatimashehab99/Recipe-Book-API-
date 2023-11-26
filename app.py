d548affa881f381c82eb1b6e8d600f3812393832
fatimashehab99 <fatima_shehab_sh@outlook.com>
2023-11-26 22:41:53 +0200

    db connection

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# DB connection
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:root@localhost/recipe_book'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# check connection
+with app.app_context():
+    try:
+        db.create_all()
+        x = "Connection to the database is succeeded!"
+
+    except Exception as e:
+        x = f"Error connecting to the database: {str(e)}"
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return x
 
 
 if __name__ == '__main__':
